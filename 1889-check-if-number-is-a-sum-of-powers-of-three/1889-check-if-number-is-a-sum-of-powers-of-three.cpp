af85dda338f4f26b60c2819dc5bbb66ac69500c9
Mukul Chauhan <mukulrajput9193@gmail.com>
2025-03-04 23:55:54 +0530

    Time: 0 ms (100%), Space: 7.7 MB (67.84%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    bool checkPowersOfThree(int n) { return checkPowersOfThreeHelper(0, n); }
+    bool checkPowersOfThree(int n) {
+        while (n > 0) {
+            // Check if this power should be used twice
+            if (n % 3 == 2) return false;
 
-private:
-    bool checkPowersOfThreeHelper(int power, int n) {
-        // Base case: if n becomes 0, we have successfully formed the sum
-        if (n == 0) return true;
+            // Divide n by 3 to move to the next greater power
+            n /= 3;
+        }
 
-        // If the current power of 3 exceeds n, we can't use it, so return false
-        if (pow(3, power) > n) return false;
-
-        // Option 1: Include the current power of 3 and subtract it from n
-        bool addPower = checkPowersOfThreeHelper(power + 1, n - pow(3, power));
-
-        // Option 2: Skip the current power of 3 and try with the next power
-        bool skipPower = checkPowersOfThreeHelper(power + 1, n);
-
-        // Return true if either option leads to a valid solution
-        return addPower || skipPower;
+        // The ternary representation of n consists only of 0s and 1s
+        return true;
     }
 };
