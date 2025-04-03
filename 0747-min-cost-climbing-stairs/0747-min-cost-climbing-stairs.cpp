7db5452de7a63ed1e991a5e559d3b93db81cef37
Mukul Chauhan <mukulrajput9193@gmail.com>
2025-04-03 21:00:41 +0530

    Time: 0 ms (100%), Space: 17.3 MB (96.69%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
+        // tabulation approach
         int n = cost.size();
         for(int i=2; i<n; i++) {
             cost[i] += min(cost[i-1], cost[i-2]);
@@ -8,3 +9,18 @@ public:
         return min(cost[n-1], cost[n-2]);
     }
 };
+
+
+// class Solution {
+// public:
+//     int helper(vector<int> & cost, int i, vector<int> &dp) {
+//         if(i==0 || i==1) return cost[i];
+//         if(dp[i] != -1) return dp[i];
+//         return dp[i] = cost[i] + min(helper(cost, i-1, dp), helper(cost, i-2, dp));
+//     }
+//     int minCostClimbingStairs(vector<int>& cost) {
+//         int n = cost.size();
+//         vector<int> dp(n, -1);
+//         return min(helper(cost, n-1, dp), helper(cost, n-2, dp));
+//     }
+// }; -> this was our memoization approach
