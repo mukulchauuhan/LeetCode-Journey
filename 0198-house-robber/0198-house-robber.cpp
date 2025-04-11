a965ba354404958cc420150cabfb97d751746a4e
Mukul Chauhan <mukulrajput9193@gmail.com>
2025-04-11 11:15:53 +0530

    Time: 0 ms (100%), Space: 10.6 MB (44.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
+vector<int> dp;
+// int fun(vector<int>& arr, int i, vector<int>& dp) {
+//     if(i==arr.size() - 1) dp[i] = arr[i]; // single house
+//     if(i==arr.size() - 2) dp[i] = max(arr[i], arr[i+1]); // two houses
+//     if(dp[i] != -1) return dp[i];
+//     return dp[i] = max(arr[i] + fun(arr,i+2, dp), 0 + fun(arr, i+1, dp));
+// }
 
-int fun(vector<int>& arr, int i, vector<int>& dp) {
-    if(i==arr.size() - 1) dp[i] = arr[i]; // single house
-    if(i==arr.size() - 2) dp[i] = max(arr[i], arr[i+1]); // two houses
-    if(dp[i] != -1) return dp[i];
-    return dp[i] = max(arr[i] + fun(arr,i+2, dp), 0 + fun(arr, i+1, dp));
+int funBottomUp(vector<int>& arr) {
+    int n = arr.size();
+    dp.clear();
+    dp.resize(n);
+    // corner case :
+    if(n==1) return arr[0];
+    // base case :
+    dp[n-1] = arr[n-1];
+    dp[n-2] = max(arr[n-1], arr[n-2]);
+
+    // tabulation implementation
+    for(int i = n-3; i >= 0; --i) {
+        dp[i] = max(arr[i]+dp[i+2], 0 + dp[i+1]);
+    }
+    return dp[0];
 }
 
     int rob(vector<int>& nums) {
-        // recursive call :
-        vector<int> dp(nums.size()+1, -1);
+        // recursive call 
+        // vector<int> dp(nums.size()+1, -1);
+        // return fun(nums, 0, dp); // we passed the array and the index. In Memoization
 
-        return fun(nums, 0, dp); // we passed the array and the index.
+        // it is the returning bottom up dp answer :
+        return funBottomUp(nums);
     }
 };                                                                                                                                                                                                                    
