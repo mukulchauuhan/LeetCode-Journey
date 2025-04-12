f92f5a7019983667dec86d1f670b1955cf44c512
Mukul Chauhan <mukulrajput9193@gmail.com>
2025-04-12 14:10:55 +0530

    Time: 4 ms (31.67%), Space: 8.3 MB (83.05%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result = "";
+        int i = 0, j = 0;
+        
+        // Merge alternately while both have characters
+        while (i < word1.length() && j < word2.length()) {
+            result += word1[i++];
+            result += word2[j++];
+        }
+        
+        // Append remaining characters (if any)
+        while (i < word1.length()) result += word1[i++];
+        while (j < word2.length()) result += word2[j++];
+
+        return result;
+    }
+};
