f0f7dbf00f980938ce5fdbda1715f2706ba2dc16
Mukul Chauhan <mukulrajput9193@gmail.com>
2025-10-08 00:17:05 +0530

    Time: 1 ms (57.72%), Space: 18.9 MB (33.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Sorting + Two PTR approach
-        nums_with_idx = [(num, i) for i, num in enumerate(nums)]
-        nums_with_idx.sort()
-        left, right = 0, len(nums_with_idx) - 1
-        while left < right:
-            curr_sum = nums_with_idx[left][0] + nums_with_idx[right][0]
-            if curr_sum == target:
-                return [nums_with_idx[left][1], nums_with_idx[right][1]]
-            elif curr_sum < target:
-                left += 1
-            else:
-                right -= 1
+        dict_nums = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dict_nums:
+                return [dict_nums[complement], i]
+            dict_nums[nums[i]] = i
         return []
