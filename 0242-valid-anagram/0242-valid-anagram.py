f721d3acaa5f98a5c50a778f1ca1a61acf7bace6
Mukul Chauhan <mukulrajput9193@gmail.com>
2025-11-05 12:17:58 +0530

    Time: 12 ms (51.54%), Space: 18.1 MB (32.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -10,4 +10,16 @@ class Solution:
         # return True
 
         # Sorting Based
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        freq = {}
+        for ch in s:
+            freq[ch] = freq.get(ch, 0)+1
+        for ch in t:
+            if ch not in freq:
+                return False
+            freq[ch] -= 1
+            if freq[ch] < 0:
+                return False
+        return True
