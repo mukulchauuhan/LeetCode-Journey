92c463a3de0dd1f59bb0cef3fc5e2d8c7fc32ffb
Mukul Chauhan <mukulrajput9193@gmail.com>
2025-11-05 12:41:39 +0530

    Time: 7 ms (89.41%), Space: 17.7 MB (86.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,4 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # if len(s) != len(t):
@@ -11,15 +12,17 @@ class Solution:
 
         # Sorting Based
         # return sorted(s) == sorted(t)
-        if len(s) != len(t):
-            return False
-        freq = {}
-        for ch in s:
-            freq[ch] = freq.get(ch, 0)+1
-        for ch in t:
-            if ch not in freq:
-                return False
-            freq[ch] -= 1
-            if freq[ch] < 0:
-                return False
-        return True
+        # if len(s) != len(t):
+        #     return False
+        # freq = {}
+        # for ch in s:
+        #     freq[ch] = freq.get(ch, 0)+1
+        # for ch in t:
+        #     if ch not in freq:
+        #         return False
+        #     freq[ch] -= 1
+        #     if freq[ch] < 0:
+        #         return False
+        # return True
+        return Counter(s) == Counter(t)
+        
